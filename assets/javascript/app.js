537930cacb2cbcd0a4daba714d3cdcc6cf184d60
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-07-02 16:07:22 -0700

    Modify buttons and change scroll tracking logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
     // Animate scroll to 
     function ascrollto(id) {
+        // 56 is the height of the navbar
         let etop = $('#' + id).offset().top - 56;
         $('html, body').animate({
             scrollTop: etop
@@ -13,16 +14,12 @@ $(document).ready(function () {
 
     // Update nav bar link depending on scroll position
     function checkScrollPos() {
-        // Top of about section including margin
-        // let aboutTop = $("#about").offset().top - 81;
-        // Top of portfolio section including margin
-        let portfolioTop = $("#portfolio").offset().top - 81;
-        // Top of contact section including margin
-        let contactTop = $("#contact").offset().top - 81;
+        // Top of portfolio section including navbar
+        let portfolioTop = $("#portfolio-title").offset().top - 56;
+        // Top of contact section including navbar
+        let contactTop = $("#contact-title").offset().top - 56.71875;
         // Top of window
         let windowpos = $(window).scrollTop();
-        console.log(windowpos);
-        console.log(portfolioTop);
 
         // If scrolled to the contact section
         if (windowpos >= contactTop) {
@@ -45,7 +42,7 @@ $(document).ready(function () {
     // When a navbar link is clicked
     $(".nav-link").on("click", function () {
         // Save section clicked
-        let section = this.text.toLowerCase();
+        let section = this.text.toLowerCase() + "-title";
         // Scroll to section
         ascrollto(section);
     });
@@ -56,48 +53,7 @@ $(document).ready(function () {
         checkScrollPos();
     });
 
-    /*
-    // TODO: change to click for mobile
-    // FIXME: Make all portfolio info disappear and portfolio images appear. Then switch "this" one
-    // When a portfolio image is moused over
-    $(".portfolio-image").on("mouseover", function () {
-        // Hide all portfolio infos
-        $(".portfolio-info").fadeOut(200, function () {
-            $(".portfolio-image").fadeIn(200);
-        });
-
-        // Show all portfolio images
-
-        // Hide the image
-        $(this).fadeOut(200, function () {
-            // Show the information
-            $(this).next().fadeIn(200);
-        });
-
-        // $(this).hide();
-        // $(this).next().show();
-    });
-
-    // TODO: change to click for mobile
-    // When the information is moused out
-    $(".portfolio-info").on("mouseleave", function () {
-        // Hide the information
-        $(this).fadeOut(200, function () {
-            // Shoe the image
-            $(this).prev().fadeIn(200);
-        });
-
-        // $(this).hide();
-        // $(this).prev().show();
-    });
-    */
-
     // Check scroll position when the document is ready
     checkScrollPos();
-
-    // $(".portfolio-info").css("height", `${$(".portfolio-image").height()}px`);
-    // $(window).resize(function () {
-    //     $(".portfolio-info").css("height", `${$(".portfolio-image").height()}px`);
-    // });
 
 });
